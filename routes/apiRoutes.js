d8935fbd0c6a8b5792f46fd72bc9ff4f6b1bae85
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-03 23:40:26 -0500

    Modified the apiRoutes.js and htmlRoutes.js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const store = require('../db/store');
 
-// GET "/api/notes" responds with all notes from the database
+// The Request Routes for the notes data
+
+// A GET request, "/api/notes", that fetches all notes from the database
 router.get('/notes', (req, res) => {
   store
     .getNotes()
@@ -11,6 +13,7 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+// A POST request, "/api/notes", that receives a new note to save to the database
 router.post('/notes', (req, res) => {
   store
     .addNote(req.body)
@@ -18,7 +21,8 @@ router.post('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+// A DELETE request, "/api/notes/:id", that receives a query parameter, in the form of a specific note's id,
+// that indicates which note should be deleted
 router.delete('/notes/:id', (req, res) => {
   store
     .removeNote(req.params.id)
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const router = require('express').Router();
 
-// "/notes" responds with the notes.html file
+// The Request Routes for the HTML files
+
+// A GET Request, "/notes", that fetches and returns the notes.html file
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// All other routes respond with the index.html file
+// All other GET requests, as represented by the asterisk, fetch and return the index.html file
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
